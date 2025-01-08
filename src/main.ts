8ed527f9be13c04a77c7c0e29579a64b03c379c9
angel zacarias <aannggeellrr@gmail.com>
2025-01-08 17:33:36 -0300

    set

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,34 @@
 import { bootstrapCameraKit } from "@snap/camera-kit";
-( async function () {
-  const camerakit = await bootstrapCameraKit({
-    apiToken:"eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM2MzU3NDU1LCJzdWIiOiI1MTU2OGUxNi1mYWFjLTQ2M2MtYmQ2Yi1mMDUzZmVhYTY1MjR-U1RBR0lOR35lNmVlNGI5Ny03YWJiLTRlMTMtOTVlMi1jNGJlMmZlOThlZTEifQ.qRqpBtBlwgcarhSGpkMA9MYiqLspBAy8D2i2yJhGIWI"
-  })
-  const liveRenderTarget = document.getElementById('canvas') as HTMLCanvasElement;
-const session = await camerakit.createSession({liveRenderTarget});
-const mediaStream = await navigator.mediaDevices.getUserMedia({
-  video:{
-    facingMode:'user'
+
+(async function initializeCameraKit() {
+  try {
+    const camerakit = await bootstrapCameraKit({
+      apiToken: "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM2MzU3NDU1LCJzdWIiOiI1MTU2OGUxNi1mYWFjLTQ2M2MtYmQ2Yi1mMDUzZmVhYTY1MjR-U1RBR0lOR35lNmVlNGI5Ny03YWJiLTRlMTMtOTVlMi1jNGJlMmZlOThlZTEifQ.qRqpBtBlwgcarhSGpkMA9MYiqLspBAy8D2i2yJhGIWI"
+    });
+
+    const liveRenderTarget = document.getElementById("canvas") as HTMLCanvasElement;
+    if (!liveRenderTarget) {
+      throw new Error("Canvas element not found");
+    }
+
+    const session = await camerakit.createSession({ liveRenderTarget });
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: "user" },
+    });
+    await session.setSource(mediaStream);
+    await session.play();
+
+    const lens = await camerakit.lensRepository.loadLens(
+      "fd13e5c0-c233-4dc1-b826-1a51eda48f51",
+      "8fa188a6-d070-4f7d-9fd0-154189e21dee"
+    );
+    if (!lens) {
+      console.warn("Lens could not be loaded");
+      return;
+    }
+
+    session.applyLens(lens);
+  } catch (error) {
+    console.error("An error occurred:", error);
   }
-})
-await session.setSource(mediaStream);
-await session.play();
-const lens = await camerakit.lensRepository.loadLens("fd13e5c0-c233-4dc1-b826-1a51eda48f51","8fa188a6-d070-4f7d-9fd0-154189e21dee")
-  session.applyLens(lens)
-})()
+})();
